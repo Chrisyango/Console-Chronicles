f8c34c7155b9595ae8606b505fcde3ac6414f4fb
Chrisyango <cyang287@yahoo.com>
2017-10-30 10:37:00 -0400

    Initial commit

---
+++ javascript/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+var openbtn = $(".openbtn")
+var closebtn = $(".closebtn")
+var nav = $(".nav")
+
+	$('body').css("display", "none");
+	$('body').fadeIn(1200);
+
+	$("#logo").hide();
+	$("#logo").delay(1000).slideDown("slow");
+
+	openbtn.click(function() {
+		nav.css("height", "100vh");
+		openbtn.delay(500).fadeOut(500);
+		closebtn.delay(800).fadeIn(500);
+	});
+
+	closebtn.click(function() {
+		nav.css("height", "0vh");
+		closebtn.delay(500).fadeOut(500);
+		openbtn.delay(800).fadeIn(500);
+	});
+
+});
