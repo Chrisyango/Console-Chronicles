e0b65d4e78100081696411a78d620a21ebd2ad4a
Chrisyango <cyang287@yahoo.com>
2017-10-30 15:34:11 -0400

    Added content section and smooth scrolling

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 var openbtn = $(".openbtn")
 var closebtn = $(".closebtn")
+var navbtn = $(".navbtn")
 var nav = $(".nav")
 
 	$('body').css("display", "none");
@@ -16,6 +17,57 @@ var nav = $(".nav")
 		nav.css("height", "0vh");
 		closebtn.delay(500).fadeOut(500);
 		openbtn.delay(800).fadeIn(500);
+	});
+
+	navbtn.click(function() {
+		nav.css("height", "0vh");
+		closebtn.delay(500).fadeOut(500);
+		openbtn.delay(800).fadeIn(500);
+	});
+
+	 // Add smooth scrolling to all links
+	$(".navbtn").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    	if (this.hash !== "") {
+      // Prevent default anchor click behavior
+    		event.preventDefault();
+
+      // Store hash
+    		var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    		$('html, body').delay(800).animate({
+    			scrollTop: $(hash).offset().top
+    		}, 1000, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+    		window.location.hash = hash;
+    		});
+    	} // End if
+	});
+
+	$(".start").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    	if (this.hash !== "") {
+      // Prevent default anchor click behavior
+    		event.preventDefault();
+
+      // Store hash
+    		var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+    		$('html, body').delay().animate({
+    			scrollTop: $(hash).offset().top
+    		}, 1000, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+    		window.location.hash = hash;
+    		});
+    	} // End if
 	});
 
 });
